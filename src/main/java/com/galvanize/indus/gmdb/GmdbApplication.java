9df2893847e2c031c794fb02a17af5cda48d06d9
Trotter <856439@cognizant.com>
2021-01-16 09:20:34 -0600

    Create GMDB database project

---
+++ src/main/java/com/galvanize/indus/gmdb/GmdbApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.indus.gmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/indus/gmdb/GmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.indus.gmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
